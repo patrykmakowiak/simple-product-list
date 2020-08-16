cb9a6122741a0b22c9a789e998bf00b558792f33
Patryk Makowiak <patryk.makowiak@gmail.com>
2020-08-16 16:48:44 +0200

    Add constant variables and routes

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+export const routes = [
+  { href: '/', name: 'Home' },
+  { href: '/products', name: 'Products' },
+  { href: '/my-account', name: 'My Account' },
+  { href: '/customer-service', name: 'Customer Service' },
+];
